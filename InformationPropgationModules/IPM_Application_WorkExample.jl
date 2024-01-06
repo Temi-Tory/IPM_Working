ae833375fcd3b1960aafb1a6d9aa69191dac156a
Temi Ohiani <63935410+Temi-Tory@users.noreply.github.com>
2024-01-06 17:39:30 +0000

    code tidy up

--- InformationPropgationModules/IPM_Application_WorkExample.jl
+++ InformationPropgationModules/IPM_Application_WorkExample.jl
@@ -1,25 +1,28 @@
 import Cairo,Fontconfig 
 using Random, Graphs, GraphMakie, GLMakie, CairoMakie, DataFrames, DelimitedFiles, Distributions
 
-Node_Priors = [] #if empty, then all nodes will have an initial reliabilty of 1.0 otherwise this should be a vector of size (number of vertices in graph) where each index  x corresponds to initial relibaility of  node x 
 
-# Define the type for the edge pair
-struct EdgePair
-    from::Int
-    to::Int
-end
+
+# Assign an empty vector to Node_Priors
+Node_Priors = Float64[]#if empty, then all nodes will have an initial reliabilty of 1.0 otherwise this should be a vector of size (number of vertices in graph) where each index  x corresponds to initial relibaility of  node x 
+
 
 # Create a dictionary to hold the reliability values
-link_reliability = Dict{EdgePair, Float64}()
+link_reliability = Dict{Information_Propagation.EdgePair, Float64}()
 
 # Read system data and create the graph
-system_data = readdlm("csvfiles/Pacific Gas and Electric (Ostrom 2004) simplified Power Distribution Network.csv", ',', header= false, Int)
+system_data = readdlm("csvfiles/KarlNetwork.csv", ',', header= false, Int)
+#system_data = readdlm("csvfiles/Shelby county gas.csv", ',', header= false, Int)
+#system_data = readdlm("csvfiles/Pacific Gas and Electric (Ostrom 2004) simplified Power Distribution Network.csv", ',', header= false, Int)
 original_system_matrix = Matrix(DataFrame(system_data, :auto))
 original_system_graph = DiGraph(original_system_matrix)
 
 # Iterate through each edge in the graph and set reliability
 for e in edges(original_system_graph)
-    link_reliability[EdgePair(src(e), dst(e))] = 0.9
-    reliability = link_reliability[EdgePair(src(e), dst(e))]
+    link_reliability[Information_Propagation.EdgePair(src(e), dst(e))] = 0.9
+    reliability = link_reliability[Information_Propagation.EdgePair(src(e), dst(e))]
 end
+
+graph,ouptput_dictionary=Information_Propagation.reliability_propagation(original_system_matrix,link_reliability,Node_Priors); ouptput_dictionary
+#output_dictionary returned a dictionary where key is node number and value is propagated reliability
 
