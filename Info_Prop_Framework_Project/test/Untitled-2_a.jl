6bf166c5347a8c16c1b3d60151315baffdcf7a93
Temi Ohiani <63935410+Temi-Tory@users.noreply.github.com>
2025-04-21 21:05:58 +0100

    fgdsfg

--- Info_Prop_Framework_Project/test/Untitled-2_a.jl
+++ Info_Prop_Framework_Project/test/Untitled-2_a.jl
@@ -5,16 +5,13 @@ using DataFrames, DelimitedFiles, Distributions,
 
 # Import framework
 using .IPAFramework
-#filepathcsv = "csvfiles/16 NodeNetwork Adjacency matrix.csv";
-#filepathcsv = "csvfiles/join_260.csv";
-#filepathcsv = "csvfiles/KarlNetwork.csv";
 
-#filepathcsv = "csvfiles/layereddiamond_2.csv";
-#filepathcsv = "csvfiles/layereddiamond_3.csv";
-#filepathcsv = "csvfiles/layereddiamond.csv";
-#filepathcsv = "csvfiles/layereddiamond_261.csv";
-#filepathcsv = "csvfiles/layereddiamond_261_plus1.csv";
-filepathcsv = "csvfiles/metro_directed_dag_for_ipm.csv";
+filepathcsv = "csvfiles/layereddiamond_3.csv";
+#filepathcsv = "csvfiles/16 NodeNetwork Adjacency matrix.csv";
+#filepathcsv = "csvfiles/KarlNetwork.csv";
+#filepathcsv = "csvfiles/Pacific Gas and Electric (Ostrom 2004) simplified Power Distribution Network.csv";
+#filepathcsv = "csvfiles/metro_directed_dag_for_ipm.csv";
+
 
 edgelist, outgoing_index, incoming_index, source_nodes, node_priors, edge_probabilities = read_graph_to_dict(filepathcsv);
 # Identify structure
@@ -25,7 +22,7 @@ iteration_sets, ancestors, descendants = find_iteration_sets(edgelist, outgoing_
 #node_priors[1]= 0.6561
 #node_priors[4]= 0.729
 # Analyze diamond structures
-diamond_structures = identify_and_group_diamonds(
+diamond_structures = #= @run  =# identify_and_group_diamonds(
 	join_nodes,
 	ancestors,
 	incoming_index,
@@ -37,8 +34,9 @@ diamond_structures = identify_and_group_diamonds(
 );
 #show(descendants[1])Set([5, 16, 20, 12, 30, 28, 8, 17, 24, 23, 19, 22, 32, 6, 11, 9, 31, 14, 3, 29, 33, 7, 25, 4, 15, 21, 2, 10, 27, 18, 13, 26])
 #show(diamond_structures)
-#show(diamond_structures[24])
+#show(diamond_structures[15])
 #show(diamond_structures[29].diamond[1].subgraph.sources)
+#show(diamond_structures[260])
 #show(diamond_structures[261])
 #show(diamond_structures[202].diamond[1].subgraph.edgelist)
 #pretty_print_diamonds(diamond_structures)
@@ -74,8 +72,8 @@ diamond_structures = identify_and_group_diamonds(
 );
 
 
-#output[261]
-#sorted_mc[29]
+#output[260]
+#output[13] #mc = 0.921...
 #= 
 
 mc_results = (MC_result(
@@ -101,7 +99,6 @@ df = DataFrame(
 
 # Add a difference column (if needed)
 df.Diff = abs.(df.AlgoValue .- df.MCValue)
-
 # Display sorted result (if you want to sort by the difference)
 show(sort(df, :Diff, rev=true), allrows=true)
 
